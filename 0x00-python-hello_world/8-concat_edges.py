ef3cad18849791a9c1e90ea4f641c9a9da7a5d10
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-09-06 18:49:53 -0500

    concat edges with single quote

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, intective, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[37:64] + " " + "with Python")
+str = str[37:64] + ' ' + 'with Python'
+print(str)
