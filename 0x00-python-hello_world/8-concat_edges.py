fe7db622d04a2e6bbd3a0fbc5e67e365f4b5b374
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-09-06 18:47:49 -0500

    concat less a line

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, intective, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[37:64] + " " + "with Python"
-print(str)
+print(str[37:64] + " " + "with Python")
