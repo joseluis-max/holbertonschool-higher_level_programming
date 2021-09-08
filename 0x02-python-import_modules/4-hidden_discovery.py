10656f1e37307e34a3db5de4bd5f5649aa86dd8e
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-09-08 13:33:17 -0500

    hidden_discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for i in range(1, len(hidden_4)):
-        print("{}".format(hidden_4[i]))
+    name = dir(hidden_4)
+    for i in name:
+        if i[:2] != '__':
+            print("{:s}".format(i))
