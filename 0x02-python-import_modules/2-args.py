cd91a717676f2da62c2143373f70dfabbcc4d7d8
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-09-08 13:20:15 -0500

    avoid exec in infinite argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     if len(sys.argv) - 1 > 1:
         print("{:d} arguments:".format(len(sys.argv) - 1))
     else:
-        print("{:d} argument:".format(len(sys.argv)- 1))
-    for i in range(1,len(sys.argv)):
+        print("{:d} argument:".format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
         print("{:d}: {}".format(i, sys.argv[i]))
