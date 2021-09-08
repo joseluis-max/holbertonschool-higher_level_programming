978c4399b9e211c654386f39ee51b2f7fb4e7eb3
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-09-07 19:08:32 -0500

    zero bug

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ else:
 print("Last digit of ", end="")
 if lD > 5:
     print("{:d} is {:d} and is greather than 5".format(number, lD))
-elif lD < 6:
+elif lD < 6 and lD != 0:
     print("{:d} is {:d} and is less than 6 and not 0".format(number, lD))
 else:
     print("{:d} is {:d} and is 0".format(number, lD))
