142e5529fbb95c3ce09300cb5a789e8644ca6bda
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-09-08 10:57:57 -0500

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) - 1 > 1:
+    print("{:d} arguments:".format(len(sys.argv) - 1))
+else:
+    print("{:d} argument:".format(len(sys.argv)- 1))
+for i in range(1,len(sys.argv)):
+    print("{:d}: {}".format(i, sys.argv[i]))
