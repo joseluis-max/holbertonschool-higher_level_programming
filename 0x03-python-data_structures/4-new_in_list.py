0750b99ef1aef08026ad3c38c6d4aed3c9e64af3
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-09-13 14:37:57 -0500

    fix error siyntax

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or len(my_list) <= inx:
+    if idx < 0 or len(my_list) <= idx:
         return my_list.copy()
     copy = my_list.copy()
-    copy[ind] = element
+    copy[idx] = element
     return copy
