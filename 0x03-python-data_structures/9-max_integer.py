fea9d350c77176a3de27c4388ccf4b15206d99a7
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-09-13 15:55:24 -0500

    max number

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    _max = 0
+    if len(my_list) == 0:
+        return None
+    for i in my_list:
+        if i > _max:
+            _max = i
+    return _max
+
+
