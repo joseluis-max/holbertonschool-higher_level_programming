4e3439783334f66bad4f2d68a809fd159966522d
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-09-13 14:33:59 -0500

    make a copy and replace

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or len(my_list) <= inx:
+        return my_list.copy()
+    copy = my_list.copy()
+    copy[ind] = element
+    return copy
