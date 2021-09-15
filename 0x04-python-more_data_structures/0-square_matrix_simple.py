5040f8c46dd4db680ef2778e9642ebd3a000a663
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-09-15 10:51:55 -0500

    squares pep8

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    n_matrix = [];
-    square = lambda x : x * x
+    n_matrix = []
     for i in matrix:
-        n_matrix.append(list(map(square, i)))
+        n_matrix.append(list(map(lambda x: x * x, i)))
     return n_matrix
