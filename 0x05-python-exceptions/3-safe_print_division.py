c1c7caef0430a3a61b57ef1d0083c2cb0a12e48a
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-09-27 15:14:43 -0500

    Divotion by zero

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    rel = 0
+    try:
+        rel = a / b
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("Inside result:{}".format(rel))
