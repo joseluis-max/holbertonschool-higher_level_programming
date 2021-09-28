564efbbdced88d410dd854d0db3a85991f206be0
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-09-27 21:14:52 -0500

    two counter

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
+    n = 0
     try:
         while i < x:
             try:
                 print("{:d}".format(my_list[i]), end="")
                 i += 1
+                n += 1
             except (ValueError, TypeError):
                 i += 1
                 continue
     except IndexError:
         raise
     print()
-    return i
+    return n
