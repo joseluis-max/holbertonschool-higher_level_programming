7c119935a4177561f9ceab625417f6d405b0eee8
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-09-28 13:27:04 -0500

    raise exceptions, default value parameter and docstring in 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Square define a square
+
+    Attributes
+    ----------
+    size: int
+        size square side
+    """
+
+    def __init__(self, size=0):
+        """
+        Parameters
+        ----------
+        size: int
+            size square side (default 0)
+        Raises
+        ------
+            TypeError: if not is a number
+            ValueError: if is less than 0
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
