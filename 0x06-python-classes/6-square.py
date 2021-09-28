3643819b55e1d9d01a2709b662e2e1e1c8bd8637
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-09-28 18:26:25 -0500

    spaces

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -116,7 +116,7 @@ class Square:
 
         if self.__size != 0:
             for n in range(0, self.__size):
-                if not self.__position[1] > 0:
+                if self.__position[1] > 0:
                     for s in range(0, self.position[0]):
                         print(end=" ")
                 for m in range(0, self.__size):
