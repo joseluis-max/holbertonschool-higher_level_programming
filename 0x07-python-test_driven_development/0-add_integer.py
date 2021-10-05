b580ee3488bdf930e603d78998a7329182897d99
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-10-05 18:03:12 -0500

    add an integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+""" Test-driven development
+    Add_integer - Arg: 
+        a: int or float number
+        b: int or float number
+"""
+
+def add_integer(a, b=98):
+    """ 
+    Return sum of two integers
+    
+    Args:
+        a: int or float (required)
+        b: int or float (default 98)
+
+    Exp: 
+        TypeError:
+            When not is a or b not is integer 
+    """
+    
+    if type(a) is int or type(a) is float:
+        if type(b) is int or type(b) is float:
+            if a == a + 1:
+                raise OverflowError("a is too large")
+            a = int(a)
+            if b == b + 1:
+                raise OverflowError("b is too large")
+            b = int(b)
+            return a + b    
+        else:
+            raise TypeError("b must be an integer")
+    else:
+        raise TypeError("a must be an integer")
