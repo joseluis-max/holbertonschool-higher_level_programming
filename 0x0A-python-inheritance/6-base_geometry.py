4c52175b4e8390d8aded4c27a951368ead7094fe
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-10-11 20:18:22 -0500

    area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" BaseGeometry """
+
+
+class BaseGeometry:
+    """ BaseClass """
+
+    def area(self):
+        """ Area definition """
+        raise Exception("area() is not implemented")
