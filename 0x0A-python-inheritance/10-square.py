831e453968bfaa5181dd13aeedb6702da7753f5d
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-10-11 21:48:54 -0500

    square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+
+#!/usr/bin/python3
+""" Class Square that inherits from rectangle class """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Class Square that inherits from rectangle class """
+
+    def __init__(self, size):
+        """ Define a new rectangle """
+        if self.integer_validator("size", size):
+            self.__size = size
+        super().__init__(size, size)
---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Class Square that inherits from rectangle class """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Class Square that inherits from rectangle class """
+
+    def __init__(self, size):
+        """ Defines a square """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """ Returns square size """
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
