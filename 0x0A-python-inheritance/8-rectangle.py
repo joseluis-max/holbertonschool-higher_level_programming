2fb636b7668bcde9777d2f035bff3fb20d6b44ca
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-10-11 21:38:22 -0500

    restangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -17,6 +17,7 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
+
 class Rectangle(BaseException):
     """ Define a Rectangle """
     def __init__(self, width, height):
