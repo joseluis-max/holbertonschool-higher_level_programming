4f8c97712573950f555d891619b04f760e8b2af2
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-10-12 22:15:04 -0500

    Student attrs

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Class Student """
+
+
+class Student:
+    """ Define Student
+        Attributes:
+            first_name: string
+            last_name: string
+            age: int
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student instance """
+        if attrs is None:
+            return self.__dict__
+        n_d = {}
+        for attr in attrs:
+            if attr in self.__dict__ and type(attr) is str:
+                n_d[attr] = self.__dict__.get(attr)
+        return n_d
