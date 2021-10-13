fe233651f54fc467271b6824ce6c02c4bb5928b3
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-10-12 21:57:45 -0500

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Write a script that adds all arguments to a Python list, and then save them to a file """
+""" Write a script that adds all arguments to a Python list
+, and then save them to a file """
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
