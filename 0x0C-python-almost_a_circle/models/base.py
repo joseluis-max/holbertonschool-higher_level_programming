a33016dead9ab4445a47898f0e39ab318d7a7f5e
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-10-15 14:39:58 -0500

    json string representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Define a Base Constructor """
+import json
 
 
 class Base:
@@ -12,3 +13,10 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return "[]"
+        if len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
