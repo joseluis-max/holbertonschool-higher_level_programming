065f6af22a5c3dd692d62600034934350531f007
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-11-23 16:05:46 -0500

    ADD: max temperature

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,5 @@
 -- Write a script that displays the max temperature of each state (ordered by State name).
 SELECT state, MAX(value)
 AS max_temp
-GROUP by state
+GROUP BY state
 ORDER BY state ASC;
