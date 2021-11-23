a74dd07eb8252f8115511bc5ba13b3dd5ea4c336
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-11-23 15:19:14 -0500

    ADD: convert to utf-8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,3 +2,4 @@
 USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE first_table ALTER COLUMN name CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
