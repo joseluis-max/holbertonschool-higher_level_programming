9279727d959f48e2abbb31644e9ec7fc26ab76fd
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-11-24 14:46:16 -0500

    ADD: create table with id not null and default value

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -6,5 +6,5 @@
 -- If the table force_name already exists, your script should not fail
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
-    name VARCHAR(254) NOT NULL
+    name VARCHAR(256) NOT NULL
 );
