196e562829cf522173aacdec235c8d0bf452137d
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-11-25 15:10:18 -0500

    ADD: list all shows and gender of this show

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -5,6 +5,6 @@
 --    You can use only one SELECT statement
 --    The database name will be passed as an argument of the mysql command
 SELECT tv_shows.title, tv_genres.name FROM tv_shows
-JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title, tv_genres.name;
