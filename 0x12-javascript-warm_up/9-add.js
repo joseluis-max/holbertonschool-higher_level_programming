8bc412ca08161c2e7c6c15cd92c56d6338164462
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-13 17:04:28 -0500

    UPDATE: sum two numbers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argv = require('process').argv;
+
+const argvInt2 = parseInt(argv[2]);
+const argvInt3 = parseInt(argv[3]);
+
+add(argvInt2, argvInt3);
+function add(a, b){
+    console.log(a + b);
+}
