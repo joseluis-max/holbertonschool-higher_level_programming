d6504cc642f91ea552a0ba55e2f1d3ac2cd37010
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-13 16:37:32 -0500

    UPDATE: print x times

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const argv = require('process').argv;
+const argvInt = parseInt(argv[2]);
+if (isNaN(argvInt)) {
+    console.log('Missing number of occurrences');
+} else if (argvInt > 0) {
+    for (let i = 0; i < argvInt; i++){
+        console.log('C is fun');
+    }
+}
