f4f1785d8f62b4471169ffc532610fb009e66e5a
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-13 17:50:52 -0500

    UPDATE: second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
 const argv = require('process').argv;
 
-let a = 0;
+let a = argv[2];
 let b = 0;
-
-for (const number of argv) {
-  if (!isNaN(parseInt(number))) {
-    if (number > a) {
+if (argv.length === 3 || argv.length === 2){
+    console.log(0);
+} else {
+  for (const number of argv) {
+    if (isNaN(parseInt(number > a))) {
       b = a;
       a = number;
     }
   }
+  console.log(b);
 }
-console.log(b);
