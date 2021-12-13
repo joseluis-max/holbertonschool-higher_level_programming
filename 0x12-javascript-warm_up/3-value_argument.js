4eea462bc16fc8cc215b5d9a63c6fd3e74192ae0
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-13 15:47:59 -0500

    UPDATE: value_argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argv = require('process').argv;
-if (argv[2] == undefined){
-    console.log('No argument');
+if (argv[2] === undefined) {
+  console.log('No argument');
 } else {
-    console.log(argv[2]);
+  console.log(argv[2]);
 }
