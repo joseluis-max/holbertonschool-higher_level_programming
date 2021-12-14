d1797439e948cb67a52112054db9faf06e83f207
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-14 17:33:55 -0500

    ADD: map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /** script that imports an array and computes a new array. */
-const list = require('100-data.js');
+const list = require('./100-data.js').list;
 const newList = list.map((value, index) => value * index);
 console.log(list);
 console.log(newList);
