620049ef15cc034330b7e571a5f9028354b08122
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-13 19:34:33 -0500

    UPDATE: using repeat

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,11 +4,7 @@ const argvInt = parseInt(argv[2]);
 if (isNaN(argvInt)) {
   console.log('Missing size');
 } else if (argvInt > 0) {
-  const draw = [];
-  for (let i = 0; i < argvInt; i++) {
-    draw.push('X');
-  }
   for (let j = 0; j < argvInt; j++) {
-    console.log(draw.join(''));
+    console.log("X".repeat(argvInt));
   }
 }
