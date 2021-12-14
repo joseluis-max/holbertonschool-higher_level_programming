af35598c2e32b38d5a6a9b90f58c6e74e36b82ad
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-14 15:18:38 -0500

    ADD: square

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+/**
+ * Represent a Square
+ * @property { number } size
+ * - size square
+ * @extends Rectangle
+ */
+class Square extends Rectangle {
+  /**
+   * Create a new Square
+   * @param { number } size - size square
+   */
+  constructor (size) {
+    super(size, size);
+    if (size > 0) {
+      this.size = size;
+    }
+  }
+}
+
+module.exports = Square;
