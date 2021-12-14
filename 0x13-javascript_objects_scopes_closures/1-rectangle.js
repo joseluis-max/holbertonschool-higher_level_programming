ce8ac0f3338a1b105a364c025ca650fc04433ca9
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-14 13:58:42 -0500

    UPDATE:constructor rectangle

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/**
+ * Represent a Rectangle
+ * @property { number } width
+ * - width rectangle
+ * @property { number } height
+ * - height rectangle
+ */
+class Rectagle {
+/**
+ * Create a new Rectangle
+ * @param { number } w - width Rectagle
+ * @param { number } h - height Rectangle
+ */
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectagle;
