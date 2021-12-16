9ac1a1b1b19a17c0900fc91219f89224c6cc8893
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-16 17:28:19 -0500

    ADD: cities by state

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Script Select with MySQLdb all rows in cities table
+"""
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cursor = db.cursor()
+    num_rows = cursor.execute("""SELECT cities.id, cities.name, states.name FROM cities 
+                                 JOIN states ON states.id = cities.state_id
+                                 ORDER BY id ASC
+                                 """)
+    rows = cursor.fetchall()
+    for r in rows:
+        print(r)
+    
+    cursor.close()
+    db.close()
