dae065c6cb5fd6e696c27257001d5c9e7ed7a5de
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-16 13:14:14 -0500

    ADD: filter state

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Script Select with MySQLdb all rows in state table the column name that start with N
+"""
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cursor = db.cursor()
+    num_rows = cursor.execute("""SELECT * FROM states WHERE name = \^N\ ORDER BY id ASC""")
+    rows = cursor.fetchall()
+    for r in rows:
+        print(r)
+    
+    cursor.close()
+    db.close()
