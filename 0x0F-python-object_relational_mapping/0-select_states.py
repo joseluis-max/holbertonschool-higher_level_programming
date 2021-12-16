5ad30e47f4722b987ca2d044697b38b97326c738
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-16 13:07:18 -0500

    UPDATE: avoid import execute

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """ Script Select with MySQLdb all rows in state table
 """
-import MySQLdb
-import sys
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
 
-
-db = MySQLdb.connect(host="localhost",
-                     user=sys.argv[1],
-                     passwd=sys.argv[2],
-                     db=sys.argv[3])
-cursor = db.cursor()
-num_rows = cursor.execute("""SELECT * FROM states ORDER BY id ASC""")
-rows = cursor.fetchall()
-for r in rows:
-    print(r)
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cursor = db.cursor()
+    num_rows = cursor.execute("""SELECT * FROM states ORDER BY id ASC""")
+    rows = cursor.fetchall()
+    for r in rows:
+        print(r)
