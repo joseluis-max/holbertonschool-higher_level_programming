f30ed40d07df34842d9e2ca4b77fd05c6e962886
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-16 16:41:11 -0500

    ADD: sql injection avoid

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
     Script Select with MySQLdb all rows in state
-    table the column name that start with N
+    table the column name with a command value argument
 """
 if __name__ == '__main__':
     import MySQLdb
