3cc0e74ee9da9be6356a75e1c1302b3e2e612f57
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-17 11:43:04 -0500

    ADD: model state my get

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" script that prints the State object with the name passed as
+    argument from the database hbtn_0e_6_usa
+    with sqlalchemy
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+if __name__ == '__main__':
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'
+                        .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    states = session.query(State).filter(State.name == sys.argv[4]).order_by(State.id).all()
+    if len(states) == 0:
+        print("Not found")
+    else:
+        for s in states:
+            print('{}: {}'.format(s.id, s.name))
+    session.close()
