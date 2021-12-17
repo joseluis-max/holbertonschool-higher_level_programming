03ab8f0eb803c5078684bb23e39d47fb270d6d3f
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-17 13:44:20 -0500

    ADD: deleting a object

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" script that deletes all State objects with a name containing the letter 
+    a from the database hbtn_0e_6_usa
+    with sqlalchemy
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+if __name__ == '__main__':
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'
+                        .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    result = session.query(State).filter(State.name.like('%a%')).all()
+    for r in result:
+        session.delete(r)
+    session.commit()
+    session.close()
