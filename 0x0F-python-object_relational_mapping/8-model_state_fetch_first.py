9a8fc49e98b9ac302f780d43aa205fc83c151455
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-17 10:21:06 -0500

    ADD: print the first id state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" script that prints the first State object from the database hbtn_0e_6_usa
+    with alchemy
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+engine = create_engine('mysql://{}:{}@localhost:3306/{}'
+                       .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+Base.metadata.create_all(engine)
+session = Session(engine)
+states = session.query(State).order_by(State.id).all()[0:1]
+for s in states:
+    print('{}: {}'.format(s.id, s.name))
+session.close()
