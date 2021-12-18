47eea1a5521f9b112c1609ca3db1178ad6da18f5
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-18 12:56:02 -0500

    UPDATE: pycodestyle

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -12,7 +12,7 @@ from sqlalchemy import create_engine
 
 if __name__ == '__main__':
     engine = create_engine('mysql://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     session = Session(engine)
     new_state = State(name = 'California')
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -2,6 +2,7 @@
 """ Create file that contains the class definition of State
     and a instance of Base = declaratibe_base() with relationship
 """
+from enum import unique
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
@@ -12,6 +13,6 @@ class State (Base):
     """ Model State define a State object with relationship
     """
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
     cities = relationship('City', cascade='all, delete', backref='state')
