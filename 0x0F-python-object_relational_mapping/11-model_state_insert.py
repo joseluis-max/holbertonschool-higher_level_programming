6c77f753658f56eae7c541fbe488ff3835945dae
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-18 12:37:28 -0500

    UPDATE: pycodestyle

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     session = Session(engine)
-    new_state = State(name='Lousiana')
+    new_state = State(name='Louisiana')
     session.add(new_state)
     session.commit()
     states = session.query(State).filter(State.name == 'Louisiana').all()
