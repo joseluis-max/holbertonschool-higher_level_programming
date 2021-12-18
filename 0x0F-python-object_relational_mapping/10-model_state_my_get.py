990e615dea77ea1f262eacd02e8590afc16fe459
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-18 12:32:18 -0500

    UPDATE: pycodestyle

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,10 +10,12 @@ from sqlalchemy import create_engine
 
 if __name__ == '__main__':
     engine = create_engine('mysql://{}:{}@localhost:3306/{}'
-                        .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     session = Session(engine)
-    states = session.query(State).filter(State.name == sys.argv[4]).order_by(State.id).all()
+    states = session.query(State)\
+                    .filter(State.name == sys.argv[4])\
+                    .order_by(State.id).all()
     if len(states) == 0:
         print("Not found")
     else:
