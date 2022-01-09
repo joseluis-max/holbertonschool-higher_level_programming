39350edcbe90e3e07fdaca9335f27ed86583465c
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-08 23:32:49 -0500

    UPDATE: header

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script that takes in a URL as an argument, sends a GET request to the URL,
 # and displays the body of the response
-curl -s -X GET "$1" -H "X-School-User-Id:98"
+curl -s -X GET -H "X-School-User-Id:98" "$1"
