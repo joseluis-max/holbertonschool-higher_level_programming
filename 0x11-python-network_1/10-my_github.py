7599a6e15054ae8711a1096a832781731bfde1dd
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-09 16:48:38 -0500

    UPDATE: some

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 """ GitHub Api"""
 import requests
-import sys
+from sys import argv
+
+
+if __name__ == "__main__":
+    user = argv[1]
+    token = argv[2]
+    url = 'https://api.github.com/user'
+
+    r = requests.get(url, auth=(user, token))
+    print(r.json().get("id"))
