b2eaeb82543624a554cb911f197b6b46843adb30
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-09 11:45:11 -0500

    UPDATE: replace tab by spaces

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,9 +7,9 @@ if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
-        print('\t- type: ', end='')
+        print('    - type: ', end='')
         print(type(html))
-        print('\t- content: ', end='')
+        print('    - content: ', end='')
         print(html)
-        print('\t- utf8 content: ', end='')
+        print('    - utf8 content: ', end='')
         print(html.decode('utf-8'))
