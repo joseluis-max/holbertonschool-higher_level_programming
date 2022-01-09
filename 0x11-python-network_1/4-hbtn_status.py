6c80cea1d9ed126c2841300f6b8dc02cf61dc7cd
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-09 13:07:21 -0500

    ADD: requests headers

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,9 +5,9 @@ import requests
 
 
 if __name__ == '__main__':
-        response = requests.get('https://intranet.hbtn.io/status')
-        print('Body response:')
-        print('    - type: ', end='')
-        print(type(response.content.decode('utf-8')))
-        print('    - content: ', end='')
-        print(response.content.decode('utf-8'))
+    response = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('    - type: ', end='')
+    print(type(response.content.decode('utf-8')))
+    print('    - content: ', end='')
+    print(response.content.decode('utf-8'))
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" script that fetches https://intranet.hbtn.io/status
+""" script that takes in a URL, sends a request to
+    the URL and displays the value of the variable
+    X-Request-Idin the response header
 """
 import requests
 import sys
