d21cf3a9ac36ae7368ee09f5a3048ca4b786fcc9
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-08 23:48:03 -0500

    ADD: find a peek

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Find a peek in a array """
+
+
+def find_peak(list_of_integers):
+    """Find a peek in a unsorted array of integers"""
+    n = len(list_of_integers)
+
+    if (n == 1):
+        return list_of_integers[0]
+    if (list_of_integers[0] >= list_of_integers[1]):
+        return list_of_integers[0]
+    if (list_of_integers[n - 1] >= list_of_integers[n - 2]):
+        return n - 1
+
+    for i in range(1, n - 1):
+        if (list_of_integers[i] >= list_of_integers[i - 1]
+           and list_of_integers[i] >= list_of_integers[i + 1]):
+            return i
