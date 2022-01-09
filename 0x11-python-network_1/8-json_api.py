d0a807b41513c27def0587560a76416b393deb3b
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-09 16:40:21 -0500

    UPDATE: some

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,19 +7,19 @@ import requests
 import sys
 
 
-if __name__ == '__main__':  
+if __name__ == '__main__':
+    if len(sys.argv[1]) == 2:
+        q = sys.argv[1]
+    else:
+        q = ""
+    res = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
     try:
-        arg = sys.argv[1]
-    except Exception:
-        arg = ""
-    q = {"q": arg}
-    req = requests.post("http://0.0.0.0:5000/search_user", data=q)
-    try:
-        result = req.json()
+        d = res.json()
+        id = d.get('id')
+        name = d.get('name')
+        if len(d) == 0 or not id or not name:
+            print("No result")
+        else:
+            print("[{}] {}".format(d.get('id'), d.get('name')))
     except Exception:
         print("Not a valid JSON")
-        exit()
-    try:
-        print("[{}] {}".format(result['id'], result['name']))
-    except Exception:
-        print("No result")
