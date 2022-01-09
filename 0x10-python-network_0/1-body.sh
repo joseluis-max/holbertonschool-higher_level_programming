aed2db68808e186bef15b755c80f326d8bb1381b
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-08 22:54:30 -0500

    UPDATE body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a GET request to the URL, and displays the body of the response
-status=$(curl -s -o /dev/null --head -w "%{http_code}" -X GET "$1")
-if [ "$status" == "200" ]; then
-    curl -s "$1"
-fi
+curl -s -X GET "$1"
