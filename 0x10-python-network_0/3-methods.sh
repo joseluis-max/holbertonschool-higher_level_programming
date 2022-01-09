9f073e4549f6719f1e9aacebb528efd22b09b3cd
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-08 22:56:42 -0500

    UPDATE: methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  takes in a URL and displays all HTTP methods the server will accept.
-curl -s -X DELETE --head $"1" | grep 'Allow:' | cut -d " " -f2-
+curl -s -X DELETE --head "$1" | grep 'Allow:' | cut -d " " -f2-
