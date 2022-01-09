36c29d0a9e8477cdc67a3572608e422c5a78841d
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-09 13:38:44 -0500

    ADD: error requests manage

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,9 +7,9 @@ import sys
 
 
 if __name__ == '__main__':
+    response = requests.get(sys.argv[1])
     try:
-        response = requests.get(sys.argv[1])
         response.raise_for_status()
         print(response.content.decode('utf-8'))
-    except requests.exceptions.HTTPError as error:
-        print(error.code)
+    except requests.exceptions.HTTPError:
+        print(response.status_code)
