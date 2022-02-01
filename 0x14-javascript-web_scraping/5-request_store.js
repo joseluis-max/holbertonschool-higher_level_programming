5d37510f49a9d819511185d8a464a65e82b5e9fa
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-02-01 14:47:32 -0500

    ADD: request and write to file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+
+request(process.argv[2], (err, response, body) => {
+  if (err) throw err;
+  fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
+    if (err) throw err;
+  });
+
+});
+
