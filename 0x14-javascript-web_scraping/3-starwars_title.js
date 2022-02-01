0a03658899f5507ccf9ac8381e5aed95043a43b2
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-02-01 13:58:24 -0500

    ADD: request and parse body for show the title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,6 +3,6 @@ const request = require('request');
 
 request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, response, body) => {
     if (err) throw err;
-    body = JSON.parse(body)
+    body = JSON.parse(body);
     console.log(body.title);
 });
