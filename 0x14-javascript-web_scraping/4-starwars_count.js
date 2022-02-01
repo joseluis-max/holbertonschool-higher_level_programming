55e48589256d71d0adf0dac6f5bb2425b6861864
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-02-01 14:16:38 -0500

    ADD: counting characters

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
-const { stripVTControlCharacters } = require('util');
 
 request(process.argv[2], (err, response, body) => {
   if (err) throw err;
   body = JSON.parse(body);
   let count = 0;
-  for (movie of body.results){
-    for (c of movie.characters){
-      if (/\/18\/$/.test(c)){
+  for (const movie of body.results) {
+    for (const c of movie.characters) {
+      if (/\/18\/$/.test(c)) {
         count++;
       }
     }
