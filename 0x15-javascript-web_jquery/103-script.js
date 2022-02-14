db888923948a08f9e6708a2ed8204f899b3bce05
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-02-14 18:25:29 -0500

    UPDATE: jquery

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,22 +1,22 @@
 /* script that fetches and prints how to say “Hello” depending on the language with enter on focus */
-function hello () {
-    const lang = $('INPUT#language_code').val();
-    if (lang) {
-        $.get(`https://www.fourtonfish.com/hellosalut/?lang=${lang}`, function (data, status) {
-            $('DIV#hello').text(data.hello);
-        });
-    } else {
-        alert('Choose a lenguage between fr, en or es');
-    }
+function hello() {
+  const lang = $('INPUT#language_code').val();
+  if (lang) {
+    $.get(`https://www.fourtonfish.com/hellosalut/?lang=${lang}`, function (data, status) {
+      $('DIV#hello').text(data.hello);
+    });
+  } else {
+    alert('Choose a lenguage between fr, en or es');
+  }
 };
 
 $(function () {
-    $('INPUT#btn_translate').click(hello);
-    $(document).on('keypress', function (event) {
-        if (event.which === 13){
-            if ($('INPUT#language_code').is(':focus')){
-                hello();
-            }
-        }
-    });
+  $('INPUT#btn_translate').click(hello);
+  $(document).on('keypress', function (event) {
+    if (event.which === 13) {
+      if ($('INPUT#language_code').is(':focus')) {
+        hello();
+      }
+    }
+  });
 });
