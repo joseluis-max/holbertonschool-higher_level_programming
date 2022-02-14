aec6267b8a45d9b13aa910741df47de35628fa61
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-02-14 11:47:13 -0500

    UPDATE: semistandard style guide

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,3 +1,3 @@
 /* Change color header to red */
-let header = document.querySelector('header');
-header.style.color = '#FF0000'
+const header = document.querySelector('header');
+header.style.color = '#FF0000';
